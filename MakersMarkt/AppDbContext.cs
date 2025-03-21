557425059e3e2f7162571f34f651ed8a90fd770e
David Rünge <d280103@icloud.com>
2025-03-21 12:37:17 +0100

    Product page added

--- MakersMarkt/AppDbContext.cs
+++ MakersMarkt/AppDbContext.cs
@@ -68,6 +68,65 @@ namespace MakersMarkt
                 new Role { Id = 2, Name = "maker" },
                 new Role { Id = 3, Name = "Admin" }
             );
+
+            modelBuilder.Entity<Category>().HasData(
+                new Category {Id=1, Name="Fiets", Description="fiets"}
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product { Id = 1, Name = "Strawberry Fields Forever", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 2, Name = "Penny Lane", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 3, Name = "Hey Jude", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 4, Name = "Let It Be", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 5, Name = "Yesterday", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 6, Name = "Come Together", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 7, Name = "Something", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 8, Name = "Here Comes the Sun", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 9, Name = "Lucy in the Sky with Diamonds", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 10, Name = "Eleanor Rigby", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 11, Name = "Yellow Submarine", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 12, Name = "A Day in the Life", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 13, Name = "All You Need Is Love", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 14, Name = "I Want to Hold Your Hand", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 15, Name = "While My Guitar Gently Weeps", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 16, Name = "With a Little Help from My Friends", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 17, Name = "Norwegian Wood", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 18, Name = "Blackbird", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 19, Name = "She Loves You", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 20, Name = "Help!", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 21, Name = "Eight Days a Week", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 22, Name = "I Am the Walrus", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 23, Name = "The Long and Winding Road", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 24, Name = "Ticket to Ride", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 25, Name = "Revolution", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 26, Name = "Love Me Do", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 27, Name = "Get Back", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 28, Name = "Ob-La-Di, Ob-La-Da", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 29, Name = "Michelle", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 30, Name = "Drive My Car", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 31, Name = "Do You Want to Know a Secret", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 32, Name = "We Can Work It Out", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 33, Name = "Magical Mystery Tour", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 34, Name = "Paperback Writer", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 35, Name = "Lady Madonna", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 36, Name = "Twist and Shout", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 37, Name = "Back in the U.S.S.R.", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 38, Name = "I Feel Fine", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 39, Name = "Baby, You're a Rich Man", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 40, Name = "And I Love Her", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 41, Name = "If I Fell", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 42, Name = "Rain", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 43, Name = "Across the Universe", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 44, Name = "You Never Give Me Your Money", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 45, Name = "Golden Slumbers", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 46, Name = "She's Leaving Home", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 47, Name = "I Saw Her Standing There", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 48, Name = "Don't Let Me Down", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 49, Name = "Sgt. Pepper's Lonely Hearts Club Band", Description = "", Price = 1.00M, CategoryId = 1 },
+new Product { Id = 50, Name = "The Fool on the Hill", Description = "", Price = 1.00M, CategoryId = 1 }
+
+                );
+
         }
     }
 }
--- MakersMarkt/Pages/ProductsPage.xaml.cs
+++ MakersMarkt/Pages/ProductsPage.xaml.cs
@@ -12,6 +12,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using MakersMarkt.Classes;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -23,9 +24,112 @@ namespace MakersMarkt.Pages
     /// </summary>
     public sealed partial class ProductsPage : Page
     {
+        private static Product SelectedProduct;
         public ProductsPage()
         {
             this.InitializeComponent();
+
+            using (var connection = new AppDbContext())
+            {
+                var Products = connection.Products;
+
+                AllProdGridView.ItemsSource = Products;
+            }
+        }
+
+        private void ReturnButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(LoginPage));
+        }
+
+        private void go2CreateViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Frame.Navigate(typeof(CreateProductPage));
+        }
+
+        private void go2EditViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Frame.Navigate(typeof(EditProductPage), SelectedProduct);
+        }
+
+        private async void deleteItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedProduct != null)
+            {
+                var deleteDialog = new ContentDialog
+                {
+                    Title = "Bevestig Verwijdering",
+                    Content = "Verwijderen in permanent en is niet ongedaan te maken (Permanent dus).",
+                    PrimaryButtonText = "Verwijder",
+                    CloseButtonText = "Annuleer",
+                    DefaultButton = ContentDialogButton.Close,
+                    XamlRoot = this.Content.XamlRoot
+                };
+
+                var result = await deleteDialog.ShowAsync();
+
+                if (result == ContentDialogResult.Primary)
+                {
+                    using (var connection = new AppDbContext())
+                    {
+                        var product2BeRemoved = connection.Products.Single(p => p.Id == SelectedProduct.Id);
+                        connection.Products.Remove(product2BeRemoved);
+                        connection.SaveChanges();
+
+                        var products = connection.Products;
+
+                        AllProdGridView.ItemsSource = products;
+                    }
+                }
+                else
+                {
+
+                }
+            }
+        }
+
+ 
+
+        private void Go2CreateViewButton_Click(object sender, RoutedEventArgs e)
+        {
+          //  Frame.Navigate(typeof(CreateProductPage));
+        }
+
+        private void Go2EditViewButton_Click(object sender, RoutedEventArgs e)
+        {
+            //Frame.Navigate(typeof(EditProductPage), SelectedProduct);
+        }
+
+        private async void DeleteItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (SelectedProduct != null)
+            {
+                var DeleteDialog = new ContentDialog
+                {
+                    Title = "Bevestig Verwijdering",
+                    Content = "Verwijderen in permanent en is niet ongedaan te maken (Permanent dus).",
+                    PrimaryButtonText = "Verwijder",
+                    CloseButtonText = "Annuleer",
+                    DefaultButton = ContentDialogButton.Close,
+                    XamlRoot = this.Content.XamlRoot
+                };
+
+                var Result = await DeleteDialog.ShowAsync();
+
+                if (Result == ContentDialogResult.Primary)
+                {
+                    using (var Connection = new AppDbContext())
+                    {
+                        var Product2BeRemoved = Connection.Products.Single(p => p.Id == SelectedProduct.Id);
+                        Connection.Products.Remove(Product2BeRemoved);
+                        Connection.SaveChanges();
+
+                        var Products = Connection.Products;
+
+                        AllProdGridView.ItemsSource = Products;
+                    }
+                }
+            }
         }
     }
 }
